a3c48d11cf31dbd239e25c3d19c835771efcd30f
suhunlin <lin.suhun@msa.hinet.net>
2023-10-03 10:35:33 +0800

    1.Add user permission setting and check 2.If user not agree send request to user

--- app/src/main/java/com/suhun/takepictures/MainActivity.java
+++ app/src/main/java/com/suhun/takepictures/MainActivity.java
@@ -1,14 +1,55 @@
 package com.suhun.takepictures;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String tag = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(isSendRequestPermissionsAboutCamera()){
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 123);
+
+        }else{
+            initCamera();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[2] == PackageManager.PERMISSION_GRANTED){//使用者同意全部的相機相關隱私設定
+                initCamera();
+            }else{
+                finish();
+            }
+        }
+    }
+    private boolean isSendRequestPermissionsAboutCamera(){
+        boolean result = false;
+        //只要有一項使用者不同意，就發送請求
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+           ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+        ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            result = true;
+        }
+
+        return result;
+    }
+    private void initCamera(){
+
     }
 }
