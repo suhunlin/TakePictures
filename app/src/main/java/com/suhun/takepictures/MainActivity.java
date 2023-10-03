ee6d98ddea5506975450805c5c5405340c3b5ef3
suhunlin <lin.suhun@msa.hinet.net>
2023-10-03 13:13:46 +0800

    Implement takePhotoSaveToDevice() method and call back function

--- app/src/main/java/com/suhun/takepictures/MainActivity.java
+++ app/src/main/java/com/suhun/takepictures/MainActivity.java
@@ -3,23 +3,34 @@ package com.suhun.takepictures;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private String tag = MainActivity.class.getSimpleName();
     private ImageView img1, img2;
+    private File saveDir;
     private ActivityResultLauncher<Intent> takePhotoResultLauncher1 =
             registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                     new ActivityResultCallback<ActivityResult>() {
@@ -32,6 +43,21 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     });
+
+    private ActivityResultLauncher<Intent> takePhotoResultLauncher2 =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if(result.getResultCode() == RESULT_OK){
+                                String filepath = new File(saveDir, "/test1.jpg").getAbsolutePath();
+                                Log.d(tag, "----filepath " + filepath + "----");
+                                Bitmap bitmap = BitmapFactory.decodeFile(filepath);
+                                img2.setImageBitmap(bitmap);
+                            }
+                        }
+                    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
     private void initCamera(){
         img1 = findViewById(R.id.lid_Image1);
         img2 = findViewById(R.id.lid_Image2);
+        saveDir = Environment.getExternalStoragePublicDirectory("HiskioPhoto");
     }
 
     public void takePhotoNotSaveToDevice(View view){
@@ -82,6 +109,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void takePhotoSaveToDevice(View view){
-
+        Uri uri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider",
+                new File(saveDir, "/test1.jpg"));
+        Log.d(tag, "----Uri " + uri + "----");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        takePhotoResultLauncher2.launch(intent);
     }
 }
